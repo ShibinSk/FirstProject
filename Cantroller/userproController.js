2202d02e8c8b18af7455a0997a46a3bb2243ae38
Shibin <shibinsunny48@gmail.com>
2022-11-24 09:10:46 +0530

    null

--- Cantroller/userproController.js
+++ Cantroller/userproController.js
@@ -2,33 +2,120 @@ const db = require("../config/connection");
 const collection = require("../config/collection");
 const ObjectId = require("mongodb").ObjectId;
 
-exports.userproget= async(req,res)=>{
+exports.userproget = async (req, res) => {
+  try {
+    const id = req.session.user._id;
+    const users = await db
+      .get()
+      .collection(collection.USER_COLLECTION)
+      .findOne({ _id: ObjectId(id) });
 
-    
+    console.log(users);
 
-    try {
+    const address = await db
+      .get()
+      .collection(collection.USER_COLLECTION)
+      .find()
+      .toArray();
 
-        const id = req.session.user._id
-        const users = await db
-        .get()
-        .collection(collection.USER_COLLECTION)
-        .findOne({_id:ObjectId(id)})
-        
-        
-        console.log(users);
+    res.render("user/userPro", {
+      navside: true,
+      users: users,
+      navside: true,
+      user: req.session.user,
+      address: address[0].address,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-        const address = await db
-        .get()
-        .collection(collection.USER_COLLECTION)
-        .find()
-        .toArray()
+exports.deleteAddress = async (req, res) => {
+  try {
+    console.log(req.query.id);
+    const userId = req.session.user._id;
+    const id = req.query.id;
 
+    const address = await db
+      .get()
+      .collection(collection.USER_COLLECTION)
+      .updateOne(
+        { _id: ObjectId(userId) },
+        { $pull: { address: { _id:id } } }
+      );
+    res.redirect("/user/userPro");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+exports.editaddress = async (req, res) => {
+  const id = req.query.id;
+  const userId = req.session.user._id;
 
-        res.render('user/userPro',{navside:true,users:users,navside:true,user:req.session.user,address:address[0].address})
-    } catch (err) {
-        console.log(err);
-        
-    }
-    
-}
+  const agg = [
+    {
+      $match: {
+        _id: new ObjectId(userId),
+      },
+    },
+    {
+      $unwind: {
+        path: "$address",
+      },
+    },
+    {
+      $match: {
+        "address._id": new ObjectId(id),
+      },
+    },
+    {
+      $project: {
+        address: 1,
+      },
+    },
+  ];
+  console.log(agg, "gttttttttttttttt");
+
+  const userdata = await db
+    .get()
+    .collection(collection.USER_COLLECTION)
+    .aggregate(agg)
+    .toArray();
+
+  console.log(userdata, "thi fuck");
+  res.render("user/edit-address", { navside: true, userdata });
+};
+
+exports.posteditaddress = async (req, res) => {
+  const addressId = req.query.id;
+
+  console.log(addressId, "aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+  console.log(req.body);
+
+  const adrs = await db
+    .get()
+    .collection(collection.USER_COLLECTION)
+    .updateOne(
+      { "address._id": ObjectId(addressId )  },
+      {
+        $set: {
+          "address.$._id": ObjectId(addressId) ,
+          "address.$.name": req.body.firstname,
+          "address.$.lnam": req.body.lastname,
+          "address.$.address": req.body.address,
+          "address.$.town": req.body.city,
+          "address.$.state": req.body.state,
+          "address.$.phone": req.body.mobile,
+          "address.$.zip": req.body.zip,
+          "address.$.email": req.body.email,
+        },
+      },
+      { upsert: true }
+    );
+
+  res.redirect("/user/userPro");
+
+ 
+};
--- routes/UserRoutes/userprofileRoutes.js
+++ routes/UserRoutes/userprofileRoutes.js
@@ -3,4 +3,11 @@ const router =express.Router();
 const userproController=require('../../Cantroller/userproController')
 
 router.get('/userPro',userproController.userproget)
+
+//===================================user Address Managment ===============================
+router.get('/delete-address',userproController.deleteAddress)
+
+router.get('/edit-address',userproController.editaddress)
+
+router.post('/edit-address',userproController.posteditaddress)
 module.exports=router;
